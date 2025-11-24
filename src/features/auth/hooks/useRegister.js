5cf6d38b1835222abf8806e6aea05c40a905c620
YuverHurtadoP <santiagohurtadopea@gmail.com>
2025-11-24 16:17:32 -0500

    erer

--- src/features/auth/hooks/useRegister.js
+++ src/features/auth/hooks/useRegister.js
@@ -1,8 +1,46 @@
 import { useMutation } from "@tanstack/react-query";
 import { registerRequest } from "../services/auth.api";
+import { useNavigate } from "react-router-dom";
 
-export const useRegister = () =>
-  useMutation({
+export const useRegister = () => {
+
+  const navigate = useNavigate();
+
+  const getToastContainer = () => {
+    let container = document.querySelector(".toast");
+    if (!container) {
+      container = document.createElement("div");
+      container.className = "toast toast-top toast-end z-50";
+      document.body.appendChild(container);
+    }
+    return container;
+  };
+
+  const registerMutation = useMutation({
     mutationFn: registerRequest,
-    onError: (err) => console.error("Error al registrarse", err),
+    onSuccess: () => {
+      getToastContainer().innerHTML = `
+        <div class="alert alert-success">
+          <span>Registro exitoso</span>
+        </div>
+      `;
+      navigate("/login");
+      setTimeout(() => {
+        getToastContainer().innerHTML = "";
+      }, 2000);
+    },
+    onError: (err) => {
+      
+      getToastContainer().innerHTML = `
+        <div class="alert alert-error">
+          <span>${err.response.data.message}</span>
+        </div>
+      `;
+      setTimeout(() => {
+        getToastContainer().innerHTML = "";
+      }, 3000);
+    }
   });
+
+  return registerMutation;
+};
